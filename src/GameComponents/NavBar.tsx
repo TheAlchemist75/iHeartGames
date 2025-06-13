4038e17c111bad67abec2e5a89cf654ed040b76b
TheAlchemist75 <Shubhammanur0075@gmail.com>
2025-06-13 17:13:23 +0200

    Implemented searching

--- src/GameComponents/NavBar.tsx
+++ src/GameComponents/NavBar.tsx
@@ -1,26 +1,31 @@
-import { HStack, Image, Text, useColorMode } from "@chakra-ui/react";
+import { Box, HStack, Image, Text, useColorMode } from "@chakra-ui/react";
 
 import logoHeart from "../assets/logoHeart.png";
-import ColorModeToggle from "./ColorModeToggle";
 import logoHeartLight from "../assets/logoHeartLight.png";
+import ColorModeToggle from "./ColorModeToggle";
+import SearchInput from "./SearchInput";
 
-const NavBar = () => {
+interface SearchInputProps {
+  onSearch: (searchText: string) => void;
+}
+
+const NavBar = ({ onSearch }: SearchInputProps) => {
   const { colorMode } = useColorMode();
-
   const logo = colorMode === "light" ? logoHeartLight : logoHeart;
-  return (
-    <>
-      <HStack padding={"1rem"} justifyContent={"space-between"}>
-        <HStack>
-          <Image src={logo} boxSize={"3.5rem"} />
-          <Text>iHeartGames</Text>
-        </HStack>
 
-        <HStack>
-          <ColorModeToggle />
-        </HStack>
+  return (
+    <HStack padding="1rem" justifyContent="space-between">
+      <HStack flex="1" spacing={4}>
+        <Image src={logo} boxSize="3.5rem" />
+        <Text whiteSpace="nowrap">iHeartGames</Text>
+
+        <Box flex="1">
+          <SearchInput onSearch={onSearch} />
+        </Box>
       </HStack>
-    </>
+
+      <ColorModeToggle />
+    </HStack>
   );
 };
 
--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return "";
+  const placeholder =
+    "https://placeholdit.com/600x400/dddddd/999999?text=No+image+available&font=roboto&font_size=26";
+  if (!url) return placeholder;
   const target = "media/";
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
